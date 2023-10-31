f669054a636db0fcfd099b81ccaabec4eed0b5f5
Angel <avegasam@gmail.com>
2023-10-30 23:06:51 -0500

    Add - landing page and kinde auth

--- micro-saas-fidelazation-program/src/app/page.tsx
+++ micro-saas-fidelazation-program/src/app/page.tsx
@@ -2,6 +2,7 @@ import { ArrowRight } from 'lucide-react';
 import MaxWidthWrapper from './components/maxWidthWrapper';
 import Link from 'next/link';
 import { buttonVariants } from '@/components/ui/button';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -31,6 +32,130 @@ export default function Home() {
           <ArrowRight className='ml-2 h-5 w-5' />
         </Link>
       </MaxWidthWrapper>
+
+      {/* value proposition section */}
+
+      <section>
+        <div className='relative isolate'>
+          <div aria-hidden="true" className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
+            <div
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+              className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]' />
+          </div>
+          <div>
+            <div className='mx-auto max-w-6xl px-6 lg:px-8'>
+              <div className='mt-16 flow-root sm:mt-24'>
+                <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+                  <Image
+                    src='/dashboard-preview.jpg'
+                    alt='product preview'
+                    width={1364}
+                    height={866}
+                    quality={100}
+                    className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            aria-hidden='true'
+            className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
+            <div
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+              className='relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]'
+            />
+          </div>
+        </div>
+      </section>
+
+      {/* Feature section */}
+      <div className='mx-auto mb-32 mt-32 max-w-5xl sm:mt-56'>
+        <div className='mb-12 px-6 lg:px-8'>
+          <div className='mx-auto max-w-2xl sm:text-center'>
+            <h2 className='mt-2 font-bold text-4xl text-gray-900 sm:text-5xl'>
+              Clientes felices en minutos
+            </h2>
+            <p className='mt-4 text-lg text-gray-600'>
+
+              Los programas de fidelización pueden ayudar a las empresas a retener a sus clientes actuales, lo que puede conducir a un aumento de los ingresos y a una reducción de los costes de adquisición de clientes.
+
+            </p>
+          </div>
+        </div>
+        {/* steps */}
+        <ol className='my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0'>
+          <li className='md:flex-1'>
+            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+              <span className='text-sm font-medium text-blue-600'>
+                Paso 1
+              </span>
+              <span className='text-xl font-semibold'>
+                Regístrate
+              </span>
+              <span className='mt-2 text-zinc-700'>
+                Ya sea comenzando con un plan gratuito o
+                elige nuestro{' '}
+                <Link
+                  href='/pricing'
+                  className='text-blue-700 underline underline-offset-2'>
+                  plan pro
+                </Link>
+                .
+              </span>
+            </div>
+          </li>
+          <li className='md:flex-1'>
+            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+              <span className='text-sm font-medium text-blue-600'>
+                Paso 2
+              </span>
+              <span className='text-xl font-semibold'>
+                Sube tu logo
+              </span>
+              <span className='mt-2 text-zinc-700'>
+                Crea una estrategia para fidelizar a tus clientes
+              </span>
+            </div>
+          </li>
+          <li className='md:flex-1'>
+            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+              <span className='text-sm font-medium text-blue-600'>
+                Paso 3
+              </span>
+              <span className='text-xl font-semibold'>
+                Comparte el enlace
+              </span>
+              <span className='mt-2 text-zinc-700'>
+
+                Administrar un catálogo de recompensas, como descuentos, productos gratuitos o experiencias exclusivas.
+
+              </span>
+            </div>
+          </li>
+        </ol>
+
+        <div className='mx-auto max-w-6xl px-6 lg:px-8'>
+          <div className='mt-16 flow-root sm:mt-24'>
+            <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+              <Image
+                src='/Dashboard-Hero-1.webp'
+                alt='uploading preview'
+                width={1419}
+                height={732}
+                quality={100}
+                className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
